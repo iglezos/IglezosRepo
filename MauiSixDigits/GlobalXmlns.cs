8c462b849e48d2e7358128c1ff1bf25df435fc30
Nikos Inglezos <n.inglezos@AcmonData.gr>
2026-02-22 08:52:01 +0200

    init commit

---
+++ MauiSixDigits/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiSixDigits")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiSixDigits.Pages")]
---
+++ MauiSixDigits/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiSixDigits
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
